f5a1c10dc1ef71c77bf5ef8522e513a4bbcddf8f
sluchyk <sloochik89@gmail.com>
2022-07-04 14:13:15 +0300

    before Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {UserModule} from "./user/user.module";
+import {NavComponent} from './nav/nav.component';
+// import {ModalService} from "./servises/modal.service";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserModule
   ],
-  providers: [],
+  // providers: [ModalService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
