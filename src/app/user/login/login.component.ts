f002de1c167e4275e11035670090ce0d8c795952
sluchyk <sloochik89@gmail.com>
2022-07-08 12:17:15 +0300

    Section 9: Intro to RxJS

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
+
+  credentials = {
+    email: '',
+    password: ''
+  }
 
   ngOnInit(): void {
   }
 
+  login() {
+    console.log(this.credentials)
+  }
+
 }
+
