366acf08b231fe93c4f506c32ecdbef641bb1009
sluchyk <sloochik89@gmail.com>
2022-07-16 13:32:31 +0300

    Section 13: Master Project: Uploading File : 204. Handling Files

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -8,6 +8,7 @@ import {Component, OnInit} from '@angular/core';
 export class UploadComponent implements OnInit {
 
   isDragover = false;
+  file: File | null = null;
 
   constructor() {
   }
@@ -16,7 +17,12 @@ export class UploadComponent implements OnInit {
   }
 
   storeFile($event: Event) {
-    this.isDragover = false
+    this.isDragover = false;
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+    if (!this.file || this.file.type !== 'video/mp4') {
+        return
+    }
+    console.log(this.file)
   }
 
 }
