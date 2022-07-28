398aeb549cac5979553aef6915bc7e977e97dba3
sluchyk89 <sluchyk.andrii@gmail.com>
2022-07-28 22:25:21 +0300

    Section 15: Master Project: Processing Videos with FFmpeg: 269. Selecting Screenshot

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -28,8 +28,8 @@ export class UploadComponent implements OnDestroy {
   percentage = 0;
   user: firebase.User | null = null;
   task?: AngularFireUploadTask;
-
   screenshots: string[] = [];
+  selectedScreenshot = ''
 
   title = new FormControl('', {
     validators: [
@@ -72,6 +72,7 @@ export class UploadComponent implements OnDestroy {
     }
 
     this.screenshots = await this.ffmpegService.getScreenshots(this.file);
+    this.selectedScreenshot = this.screenshots[0];
 
     this.title.setValue(
       this.file.name.replace(/\.[^/.]+$/, '')
