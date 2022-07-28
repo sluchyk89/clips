3dd63c3f3f10531eb7e2c02b0ae32daabe611cb9
sluchyk89 <sluchyk.andrii@gmail.com>
2022-07-28 20:23:19 +0300

    Section 15: Master Project: Processing Videos with FFmpeg: 263. Saving Files in Memory

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -56,7 +56,7 @@ export class UploadComponent implements OnDestroy {
     this.task?.cancel()
   }
 
-  storeFile($event: Event) {
+  async storeFile($event: Event) {
 
     this.isDragover = false;
     this.file = ($event as DragEvent).dataTransfer ?
@@ -66,6 +66,9 @@ export class UploadComponent implements OnDestroy {
     if (!this.file || !(this.file.type === 'video/mp4' || this.file.type === 'video/webm')) {
       return
     }
+
+    this.ffmpegService.getScreenshots(this.file)
+
     this.title.setValue(
       this.file.name.replace(/\.[^/.]+$/, '')
     )
