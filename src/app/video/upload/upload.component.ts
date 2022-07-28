b6ae92836e0b6e4976ff8971883fcb068a7622e6
sluchyk89 <sluchyk.andrii@gmail.com>
2022-07-28 23:16:21 +0300

    Section 15: Master Project: Processing Videos with FFmpeg: 272. Recalculating the Upload Progress

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -2,7 +2,7 @@ import {Component, OnDestroy} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AngularFireStorage, AngularFireUploadTask} from "@angular/fire/compat/storage";
 import {v4 as uuid} from 'uuid';
-import {last, switchMap} from "rxjs";
+import {combineLatest, last, switchMap} from "rxjs";
 import {AngularFireAuth} from "@angular/fire/compat/auth";
 import firebase from "firebase/compat/app";
 import {ClipService} from "../../services/clip.service";
@@ -103,8 +103,16 @@ export class UploadComponent implements OnDestroy {
 
     this.screenshotTask = this.storage.upload(screenshotPath, screenshotBlob);
 
-    this.task.percentageChanges().subscribe(progress => {
-      this.percentage = progress as number / 100
+
+    combineLatest([this.task.percentageChanges(), this.screenshotTask.percentageChanges()]).subscribe((progress) => {
+      const [clipProgress, screenshotProgress] =  progress;
+      if (!clipProgress || !screenshotProgress) {
+        return
+      }
+
+      const total = clipProgress + screenshotProgress;
+
+      this.percentage = total as number / 200
     })
 
     this.task.snapshotChanges().pipe(
