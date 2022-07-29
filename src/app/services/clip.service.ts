64b2cea406aecc429887967ca46e3d2f393dbbc7
sluchyk89 <sluchyk.andrii@gmail.com>
2022-07-30 00:17:54 +0300

    Section 16: Master Project: Playing Video: 278. Querying for Clips

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -16,6 +16,8 @@ import {AngularFireStorage} from "@angular/fire/compat/storage";
 })
 export class ClipService {
   public clipCollection: AngularFirestoreCollection<IClip>
+  public pageClips: IClip[] = [];
+  pendingReq = false;
 
   constructor(
     private db: AngularFirestore,
@@ -65,5 +67,40 @@ export class ClipService {
       }),
       map(snapshot => (snapshot as QuerySnapshot<IClip>).docs)
     )
+  }
+
+  async getClips() {
+    if (this.pendingReq) {
+      return
+    }
+
+    this.pendingReq = true
+
+    let query = this.clipCollection.ref.orderBy('timestamp', 'desc')
+      .limit(3)
+
+
+    const {length} = this.pageClips;
+
+    if (length) {
+      const lastDocId = this.pageClips[length - 1].docID
+      const lastDoc = await this.clipCollection.doc(lastDocId)
+        .get()
+        .toPromise()
+
+      query = query.startAfter(lastDoc)
+    }
+
+    const snapshot = await query.get();
+
+    snapshot.forEach(doc => {
+      this.pageClips.push({
+          docID: doc.id,
+          ...doc.data()
+        }
+      )
+    })
+
+    this.pendingReq = false
   }
 }
