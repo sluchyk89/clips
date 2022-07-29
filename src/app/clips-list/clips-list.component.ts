eb67f33e344aa5bf08df90aa9e90bc38ce4d27eb
sluchyk89 <sluchyk.andrii@gmail.com>
2022-07-29 23:55:52 +0300

    Section 16: Master Project: Playing Video: 277. Handling Scrolling Event

--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-clips-list',
   templateUrl: './clips-list.component.html',
   styleUrls: ['./clips-list.component.scss']
 })
-export class ClipsListComponent implements OnInit {
+export class ClipsListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+
+  handleScroll = () => {
+
+    const {scrollTop, offsetHeight} = document.documentElement;
+    const {innerHeight} = window;
+    const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight
+
+    if (bottomOfWindow) {
+      console.log('bottom of window')
+    }
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
 }
