e57ee6ff449984c483d225423d9fb4543f4c1333
sluchyk89 <sluchyk.andrii@gmail.com>
2022-07-30 12:43:50 +0300

    Section 16: Master Project: Playing Video: 287. Style without View Encapsulation

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,11 +1,12 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
 import videojs from "video.js";
 
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
-  styleUrls: ['./clip.component.scss']
+  styleUrls: ['./clip.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ClipComponent implements OnInit {
 
