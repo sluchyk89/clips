9c98f4bef918ca37f5c063a613581fdc45e63f43
sluchyk89 <sluchyk.andrii@gmail.com>
2022-08-11 22:38:18 +0300

    Section 18: Master Project: Testing 303. Querying the Component’s Template

--- src/app/shared/tab/tab.component.spec.ts
+++ src/app/shared/tab/tab.component.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { TabComponent } from './tab.component';
+import {TabComponent} from './tab.component';
+import {By} from "@angular/platform-browser";
 
 describe('TabComponent', () => {
   let component: TabComponent;
@@ -8,9 +9,9 @@ describe('TabComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TabComponent ]
+      declarations: [TabComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(TabComponent);
     component = fixture.componentInstance;
@@ -20,4 +21,14 @@ describe('TabComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have .hidden class', () => {
+    // tree possible selectors
+    // first solution the best one
+    const element = fixture.debugElement.query(By.css('.hidden'));
+    const element2 = fixture.nativeElement.querySelector('.hidden');
+    const element3 = document.querySelector('.hidden');
+
+    expect(element).toBeTruthy();
+  })
 });
