1295df7cd5f11cae2366762a8608ce524b918349
sluchyk89 <sluchyk.andrii@gmail.com>
2022-08-13 12:26:12 +0300

    Section 18: Master Project: Testing 308. Mocking Service

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -1,16 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { NavComponent } from './nav.component';
+import {NavComponent} from './nav.component';
+import {of} from "rxjs";
 
 describe('NavComponent', () => {
   let component: NavComponent;
   let fixture: ComponentFixture<NavComponent>;
+  let mockedAuthService = jasmine.createSpyObj(
+    'AuthService',
+    ['createUser', 'logout'],
+    {
+      isAuthenticated$: of(true),
+    }
+  )
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
+      declarations: [NavComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(NavComponent);
     component = fixture.componentInstance;
