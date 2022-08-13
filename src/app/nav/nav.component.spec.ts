47fb1aaf4d079e6221d7af07c3723297cb73b053
sluchyk89 <sluchyk.andrii@gmail.com>
2022-08-13 12:34:31 +0300

    Section 18: Master Project: Testing 308. Overriding a Dependency

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -2,6 +2,7 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {NavComponent} from './nav.component';
 import {of} from "rxjs";
+import {AuthService} from "../services/auth.service";
 
 describe('NavComponent', () => {
   let component: NavComponent;
@@ -16,7 +17,10 @@ describe('NavComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [NavComponent]
+      declarations: [NavComponent],
+      providers: [
+        {provide: AuthService, useValue: mockedAuthService}
+      ]
     })
       .compileComponents();
 
